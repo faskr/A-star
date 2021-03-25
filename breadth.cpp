6bbf42d1006767e06cb5aaffdf448e9477136ce5
faskr <faskr@umich.edu>
2021-03-25 02:27:38 -0400

    breadth implemented

--- breadth.cpp
+++ breadth.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <queue>
+#include <deque>
 #include <math.h>
 #include <algorithm>
 #include <string>
@@ -10,8 +10,15 @@
 
 using std::cout;
 using std::endl;
+using std::deque;
 using std::vector;
 using std::string;
+
+struct point {
+    int x;
+    int y;
+    //int z;
+};
 
 void travel(int cur, int next) {
     uint32_t x = 1;
@@ -20,8 +27,80 @@ void travel(int cur, int next) {
     }
 }
 
+bool in_bounds(int x, int y, int width, int height) {
+    return 0 <= x && x < width && 0 <= y && y < height;
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 2) return 0;
     std::ifstream ifs(argv[1]);
-    
+    int height;
+    int width;
+    point pos;
+    point goal;
+    ifs >> height;
+    ifs >> width;
+    ifs >> pos.x;
+    ifs >> pos.y;
+    ifs >> goal.x;
+    ifs >> goal.y;
+    vector<vector<double>> map(width);
+    vector<vector<point>> prev(width);
+    vector<vector<int>> reached(width);
+    for (int i = 0; i < width; i++) {
+        map[i].resize(height);
+        prev[i].resize(height);
+        reached[i].resize(height);
+        for (int j = 0; j < height; j++) {
+            ifs >> map[i][j];
+        }
+    }
+    deque<point> border;
+    reached[pos.x][pos.y] = true;
+    border.push_back(pos);
+    point cur_pt;
+    while (!reached[goal.x][goal.y]) {
+        cur_pt = border.front();
+        border.pop_front();
+        if (in_bounds(cur_pt.x, cur_pt.y + 1, width, height) && !reached[cur_pt.x][cur_pt.y + 1]) {
+            reached[cur_pt.x][cur_pt.y + 1] = true;
+            border.push_back({cur_pt.x, cur_pt.y + 1});
+            prev[cur_pt.x][cur_pt.y + 1] = cur_pt;
+        }
+        if (in_bounds(cur_pt.x, cur_pt.y - 1, width, height) && !reached[cur_pt.x][cur_pt.y - 1]) {
+            reached[cur_pt.x][cur_pt.y - 1] = true;
+            border.push_back({cur_pt.x, cur_pt.y - 1});
+            prev[cur_pt.x][cur_pt.y - 1] = cur_pt;
+        }
+        if (in_bounds(cur_pt.x + 1, cur_pt.y, width, height) && !reached[cur_pt.x + 1][cur_pt.y]) {
+            reached[cur_pt.x + 1][cur_pt.y] = true;
+            border.push_back({cur_pt.x + 1, cur_pt.y});
+            prev[cur_pt.x + 1][cur_pt.y] = cur_pt;
+        }
+        if (in_bounds(cur_pt.x - 1, cur_pt.y, width, height) && !reached[cur_pt.x - 1][cur_pt.y]) {
+            reached[cur_pt.x - 1][cur_pt.y] = true;
+            border.push_back({cur_pt.x - 1, cur_pt.y});
+            prev[cur_pt.x - 1][cur_pt.y] = cur_pt;
+        }
+    }
+    cur_pt = goal;
+    deque<point> path;
+    while (cur_pt.x != pos.x || cur_pt.y != pos.y) {
+        path.push_front(cur_pt);
+        cur_pt = prev[cur_pt.x][cur_pt.y];
+    }
+    path.push_front(cur_pt);
+    // print
+    cout << "path coordinates:\n";
+    for (point pt : path) {
+        reached[pt.x][pt.y] = 2;
+        cout << pt.x << ',' << pt.y << '\n';
+    }
+    cout << "\npath map:\n";
+    for (int i = height - 1; i >= 0; i--) {
+        for (int j = 0; j < width; j++) {
+            cout << reached[j][i] << ' ';
+        }
+        cout << '\n';
+    }
 }
