f412ff11b562d003365c24a6c81eb72605e591ee
faskr <faskr@umich.edu>
2021-03-25 00:35:35 -0400

    reorganized

---
+++ heuristic.cpp
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+#include <string>
+#include <sstream>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+void travel(int cur, int next) {
+    uint32_t x = 1;
+    while (x != 0) {
+        x++;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) return 0;
+    std::ifstream ifs(argv[1]);
+    
+}
