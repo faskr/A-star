9432926ba735da693b23169d69201fdc3e22d6bd
faskr <faskr@umich.edu>
2021-04-10 04:47:23 -0400

    organization

--- Astar2d.cpp
+++ Astar2d.cpp
@@ -6,96 +6,112 @@
 #include <algorithm>
 
 using std::cout;
-using std::endl;
 using std::deque;
 using std::vector;
 using std::string;
 
+class Astar;
+
 struct point {
+    Astar* A;
     int x;
     int y;
 };
 
+// TODO: put everything related to class in header file
+// TODO: consider renaming Astar class or instances to path (or AstarPath) or journey or a synonym
 class Astar {
 public:
-    struct Info {
-        double map;
-        double cost;
-        point prev;
-        int added;
-        int visited;
-    };
-
-    static vector<vector<Info>> info;
-    static point goal;
-
-    Astar() {}
-
-    Astar(int h, int w, point p) : height(h), width(w), pos(p) {
+    Astar(int h, int w, point p, point g) : height(h), width(w), pos(p), goal(g) {
         info.resize(width);
         for (int i = 0; i < width; i++) {
-            info[i].resize(height, {0, std::numeric_limits<double>::max(), 0, 0, 0});
+            info[i].resize(height, {{0, 0, 0}, -1, std::numeric_limits<double>::max(), 0, 0});
         }
     }
 
-    bool in_bounds(int x, int y, int width, int height) {
-        return 0 <= x && x < width && 0 <= y && y < height;
+    void print_path();
+    void print_cost_map();
+    void print_search_map();
+    void move() { pos = goal; }
+    void set_block(point p, double cost) { info[p.x][p.y].map = cost; }
+    point get_pos() { return pos; }
+    point get_goal() { return goal; }
+    deque<point> find_path();
+    deque<point> get_path() { return path; }
+    double block_cost(point p) { return info[p.x][p.y].map; }
+    double move_cost(point p) { return info[p.x][p.y].cost; }
+
+    bool in_bounds(point p) {
+        return 0 <= p.x && p.x < width && 0 <= p.y && p.y < height;
     }
 
     int heuristic(point p1, point p2) {
         return abs(p1.x - p2.x) + abs(p1.y - p2.y); // Manhattan distance
     }
 
-    int output_path(int i, int j) {
-        if (info[j][i].visited == 2) return 3;
-        return output_search(i, j);
-    }
-
-    int output_search(int i, int j) {
-        return info[j][i].visited ? info[j][i].visited * 2 : info[j][i].added;
-    }
-
-    int output_cost(int i, int j) {
-        return info[j][i].cost == std::numeric_limits<double>::max() ? -1 : info[j][i].cost;
-    }
-
-    void print_map(string type, int (Astar::*output)(int, int), char delim);
-    void print_coords();
-
-    deque<point> find_path();
-
 private:
+    struct Info {
+        point prev;
+        double map;
+        double cost;
+        int added;
+        int visited;
+    };
+
+    vector<vector<Info>> info;
     vector<point> border;
     deque<point> path;
     point pos;
+    point goal;
     point cur_pt;
     int width;
     int height;
 
+    int output_search(point p) {
+        if (info[p.x][p.y].visited == 2) return 3;
+        return info[p.x][p.y].visited ? info[p.x][p.y].visited * 2 : info[p.x][p.y].added;
+    }
+
+    int output_cost(point p) {
+        
+        return info[p.x][p.y].cost == std::numeric_limits<double>::max() ? -1 : info[p.x][p.y].cost;
+    }
+
     void update_neighbors();
 };
 
-vector<vector<Astar::Info>> Astar::info;
-point Astar::goal;
-
-class cheaper : public Astar {
+class cheaper {
 public:
     bool operator() (point p1, point p2) {
-        return info[p1.x][p1.y].cost + heuristic(p1, goal) > info[p2.x][p2.y].cost + heuristic(p2, goal);
+        if (p1.A != p2.A) {
+            cout << "error: two points from different path searches compared\n";
+            exit(1);
+        }
+        double p1_total = p1.A->move_cost(p1) + p1.A->heuristic(p1, p1.A->get_goal());
+        double p2_total = p2.A->move_cost(p2) + p2.A->heuristic(p2, p2.A->get_goal());
+        return p1_total > p2_total;
     }
 };
 
-void Astar::print_map(string type, int (Astar::*output)(int, int), char delim) {
-    cout << '\n' << type << " map:\n";
+void Astar::print_cost_map() {
+    cout << "\ncost map:\n";
     for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            cout << (this->*output)(i, j) << delim;
-        }
+        for (int j = 0; j < width; j++)
+            cout << output_cost({ this, j, i }) << '\t';
         cout << '\n';
     }
 }
 
-void Astar::print_coords() {
+void Astar::print_search_map() {
+    cout << "\nsearch map:\n";
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++)
+            cout << output_search({ this, j, i }) << ' ';
+        cout << '\n';
+    }
+}
+
+void Astar::print_path() {
     cout << "\npath coordinates:\n";
     for (point pt : path) {
         info[pt.x][pt.y].visited = 2;
@@ -105,15 +121,15 @@ void Astar::print_coords() {
 
 void Astar::update_neighbors() {
     vector<point> sides{4};
-    sides[0] = {cur_pt.x, cur_pt.y + 1};
-    sides[1] = {cur_pt.x, cur_pt.y - 1};
-    sides[2] = {cur_pt.x + 1, cur_pt.y};
-    sides[3] = {cur_pt.x - 1, cur_pt.y};
+    sides[0] = {this, cur_pt.x, cur_pt.y + 1};
+    sides[1] = {this, cur_pt.x, cur_pt.y - 1};
+    sides[2] = {this, cur_pt.x + 1, cur_pt.y};
+    sides[3] = {this, cur_pt.x - 1, cur_pt.y};
     double new_cost;
     bool new_cost_less;
     for (point side : sides) {
         // update cost if block is not visited and new cost is less than existing
-        if (!in_bounds(side.x, side.y, width, height)) continue;
+        if (!in_bounds(side)) continue;
         new_cost = info[cur_pt.x][cur_pt.y].cost + info[side.x][side.y].map;
         new_cost_less = new_cost < info[side.x][side.y].cost;
         if (!info[side.x][side.y].visited && new_cost_less) {
@@ -122,7 +138,7 @@ void Astar::update_neighbors() {
         }
         // push to border if not added already
         if (!info[side.x][side.y].added) {
-            border.push_back({side.x, side.y});
+            border.push_back({this, side.x, side.y});
             std::push_heap(border.begin(), border.end(), cheaper());
             info[side.x][side.y].added = true;
         }
@@ -141,8 +157,8 @@ deque<point> Astar::find_path() {
         border.pop_back(); // remove current point from border
         update_neighbors(); // update costs and add to border as needed
         std::make_heap(border.begin(), border.end(), cheaper()); // update in case element costs updated
-        print_map("search", &Astar::output_search, ' ');
-        print_map("cost", &Astar::output_cost, '\t');
+        print_search_map();
+        print_cost_map();
     }
     cur_pt = goal;
     while (cur_pt.x != pos.x || cur_pt.y != pos.y) {
@@ -150,8 +166,8 @@ deque<point> Astar::find_path() {
         cur_pt = info[cur_pt.x][cur_pt.y].prev;
     }
     path.push_front(cur_pt);
-    print_coords();
-    print_map("path", &Astar::output_path, ' ');
+    print_path();
+    print_search_map();
     return path;
 }
 
@@ -160,13 +176,14 @@ int import_and_run(string filename) {
     int height, width;
     point pos, goal;
     ifs >> height >> width >> pos.x >> pos.y >> goal.x >> goal.y;
-    Astar A(height, width, pos);
-    if (!A.in_bounds(pos.x, pos.y, width, height)) return 1;
-    if (!A.in_bounds(goal.x, goal.y, width, height)) return 1;
-    A.goal = goal;
+    Astar A(height, width, pos, goal);
+    if (!A.in_bounds(pos)) return 1;
+    if (!A.in_bounds(goal)) return 1;
+    double cost;
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            ifs >> A.info[j][i].map;
+            ifs >> cost;
+            A.set_block({ &A, j, i }, cost);
         }
     }
     A.find_path();
